0084e163d4fdd8cb8f3894a1659bdcc7495aa966
Gnaneshnayak <mr.gnaneshnayak@gmail.com>
2025-11-25 19:09:05 +0530

    initial setup for backend and frontend

---
+++ packages/server/index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+import type { Request, Response } from 'express';
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(process.env.OPENAI_API_KEY);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
